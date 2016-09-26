3d509852e52b001cfe11e61975ab0aabf5d39976
Liam <lhal201@aucklanduni.ac.nz>
2016-09-26 23:40:51 +0000

    Unit 1 and Unit 2

---
+++ Unit_2/script.js
@@ -0,0 +1,7 @@
+console.log("ready about to start")
+$(document).ready(function() {
+    $('.pull-me').click(function() {
+    $('.panel').slideToggle('slow')
+    });
+console.log("ready has completed")
+});
